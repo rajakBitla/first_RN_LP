74afba20c2cda348e7b5453c785edad58a7f99dd
rajak <rajakumar@bitlasoft.com>
2025-12-13 18:10:55 +0530

    added searching tab for searching photo

--- compponents/PhotoGalary.js
+++ compponents/PhotoGalary.js
@@ -1,7 +1,8 @@
-import { StyleSheet, View, Text, FlatList, Dimensions } from "react-native";
+import { StyleSheet, View, Text, FlatList, Dimensions ,TextInput} from "react-native";
 import { Colors } from "../Colors/color";
 import CardImage from "../compponents/ImageCard";
 import { Photos } from "../data/Photos";
+import { useState } from "react";
 
 const screenWidth = Dimensions.get('window').width;
 const padding = 16;
@@ -10,31 +11,37 @@ const numColumns = 2;
 const cardSize = (screenWidth - padding * 2 - spacing * (numColumns - 1)) / numColumns;
 
 function PhotoGallery() {
+    const [filteredPhotos, setFilteredPhotos] = useState(Photos);
+    function filterPhotos(text){
+       setFilteredPhotos(Photos.filter(photo => photo.title.toLowerCase().includes(text.toLowerCase())));
+    }
     return (
         <>
         <View style={styles.horizontalcontainer}>
             <Text style={styles.headerText}>Photo Gallery</Text>
+            <TextInput style={styles.searchInput} placeholder="Search photos..."  placeholderTextColor={Colors.primary} onChangeText={filterPhotos} />
             {/* horizontal scrolling */}
-            <FlatList
-                data={Photos}
+         {filteredPhotos.length > 0 &&   <FlatList
+                data={filteredPhotos}
                 horizontal={true}    
                 showsHorizontalScrollIndicator={false} 
                 renderItem={({ item }) => <CardImage photo={item} cardSize={120} />}
                 keyExtractor={(item) => item.id}
                 contentContainerStyle={{ paddingHorizontal: padding }}
                 ItemSeparatorComponent={() => <View style={{ width: 12 }} />} 
-            />
+            />}
         </View>
         <View style={styles.container}>
             {/* Vertical scrolling */}
-            <FlatList
-                data={Photos}
+          {filteredPhotos.length > 0 &&     <FlatList
+                data={filteredPhotos}
                 numColumns={numColumns}
                 renderItem={({ item }) => <CardImage photo={item} cardSize={cardSize} />}
                 keyExtractor={(item) => item.id}
                 columnWrapperStyle={{ justifyContent: 'space-between', marginBottom: spacing }}
                 contentContainerStyle={{ padding: padding }}
-            />
+            /> }
+            {filteredPhotos.length === 0 && <Text style={styles.noPhotosText}>No photos found.</Text>}
         </View>
         
         </>
@@ -59,4 +66,20 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         marginVertical: 16,
     },
+    searchInput: {
+        height: 40,
+        width: '95%', 
+        borderColor: Colors.card,
+        borderWidth: 1,
+        borderRadius: 8,
+        paddingHorizontal: 12,
+        marginHorizontal: 16,
+        marginBottom: 16,
+        color: Colors.primary,
+        backgroundColor: Colors.muted,
+    },
+    noPhotosText: {
+        color:Colors.text, 
+        textAlign:'center', 
+        marginTop:20}
 });
